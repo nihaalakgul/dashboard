fc446e07896b4ecda5f4d5a96633a77c61760093
Nihal Akgül <nihaalakgul@outlook.com>
2025-09-12 09:08:57 +0300

    Initial Commit

---
+++ dashboard/dashboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  dashboardApp.swift
+//  dashboard
+//
+//  Created by Nihal Akgül on 12.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct dashboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ dashboardTests/dashboardTests.swift
@@ -0,0 +1,17 @@
+//
+//  dashboardTests.swift
+//  dashboardTests
+//
+//  Created by Nihal Akgül on 12.09.2025.
+//
+
+import Testing
+@testable import dashboard
+
+struct dashboardTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
